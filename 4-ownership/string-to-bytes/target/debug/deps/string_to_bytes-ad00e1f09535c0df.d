9e59a292ede124399d278d9031d6db0f24f04b42
“DiorChoppa” <dvoryadkinka@mos.ru>
2023-10-05 01:44:50 +0300

    added string to bytes

---
+++ 4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ad00e1f09535c0df.d
@@ -0,0 +1,5 @@
+/home/blssd/dev/Rust/the-rust-programming-language/4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ad00e1f09535c0df.rmeta: src/main.rs
+
+/home/blssd/dev/Rust/the-rust-programming-language/4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ad00e1f09535c0df.d: src/main.rs
+
+src/main.rs:
---
+++ 4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ed599dc41c9e8ce3.d
@@ -0,0 +1,5 @@
+/home/blssd/dev/Rust/the-rust-programming-language/4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ed599dc41c9e8ce3.rmeta: src/main.rs
+
+/home/blssd/dev/Rust/the-rust-programming-language/4-ownership/string-to-bytes/target/debug/deps/string_to_bytes-ed599dc41c9e8ce3.d: src/main.rs
+
+src/main.rs:
